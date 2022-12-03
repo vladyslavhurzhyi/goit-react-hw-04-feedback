e7a62f859284a438c115e153533b8c5fff0a10b4
Vladyslav Hurzhyi <soulline0@gmail.com>
2022-12-03 22:55:15 +0100

    fix pj

--- src/components/FeedbackOpt/FeedbackOptions.jsx
+++ src/components/FeedbackOpt/FeedbackOptions.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Btn } from '../FeedbackOtpions.styled';
+import { Btn } from './FeedbackOtpions.styled';
 
 export const FeedbackOptions = ({ options, onLeaveFeedback }) => {
   return options.map(item => {
